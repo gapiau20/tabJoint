746e2ca73557cd4dbaa5f53f97428beda605fe28
Pierre Aublin <pierre.aublin@uni-a.de>
2026-02-10 11:56:52 +0100

    Improve argparse

--- read.py
+++ read.py
@@ -1,15 +1,28 @@
 import pandas as pd
 from functools import reduce
 from pathlib import Path
+import os
+import argparse
 
+def get_readers(**kwargs):
+    return{
+            '.csv':lambda x:pd.read_csv(x,**kwargs),
+            '.tsv':lambda x:pd.read_tsv(x,**kwargs),
+            '.xlsx':lambda x:pd.read_excel(x,**kwargs),
+            '.xls':lambda x:pd.read_excel(x,**kwargs),
+        }
 
+def extract_files(input_directory):
+    dir_path=Path(input_directory)
+    if not os.path.isdir(input_directory):
+        raise ValueError('{} is not a valid directory'.format(input_directory))
+    allowed_exts=set(get_readers().keys())
+
+    files=[str(p)for p in dir_path.rglob("*")if p.suffix.lower() in allowed_exts]
+    return files
+    
 def read_file(file:str,**kwargs):
-    readers={
-        '.csv':lambda x:pd.read_csv(x,**kwargs),
-        '.tsv':lambda x:pd.read_tsv(x,**kwargs),
-        '.xlsx':lambda x:pd.read_excel(x,**kwargs),
-        '.xls':lambda x:pd.read_excel(x,**kwargs),
-    }
+    readers=get_readers(**kwargs)
     extension=Path(file).suffix.lower()
 
     if extension in readers.keys():
@@ -51,10 +64,32 @@ def merge_files(file_paths, output_path, patient_col="Patient"):
     merged_df.to_csv(output_path, index=False)
     return merged_df
 
+def main():
+    parser=argparse.ArgumentParser(
+        description="Merge multiple tabular datasets into a csv file"
+    )
+    parser.add_argument("-i",'--inputs', nargs='+',help='Input files to merge')
+    parser.add_argument("-d", "--directory",help="Directory containing input files")
+    parser.add_argument('-o','--output',required=True,help='Output file')
+    parser.add_argument('--joint-col',default='Patient',help='Name of the column to joint the files (default: Patient)')
+    parser.add_argument('-v','--verbose',action="store_true",help='Whether to display the extracted files')
+    args=parser.parse_args() 
 
+    if args.directory: 
+        inputs=extract_files(args.directory)
+    elif args.inputs:
+        inputs=args.inputs
+    else: 
+        raise ValueError('At least directory (-d) or a list of input files should be specified') 
+    
+    if args.verbose: 
+        print('Extracted {} files: \n {}'.format(len(inputs),str(inputs)))
+    merge_files(inputs,args.output,args.joint_col)
+    return
 
 if __name__=='__main__':
-    files = ["test_data\\table1.csv","test_data\\table2.csv","test_data\\table3.csv"]
-    final_df = merge_files(file_paths=files,output_path="test_data\\patients_final.csv")
-    print(final_df)
+    main()
+    # print(extract_files('test_data'))
+    # merge_files(extract_files('test_data'),'output.csv')
+
 
